e76099e1d881fcf784bf0ac4518c4f483fc07c76
Drew <drew@mybrandhero.co>
2022-03-28 09:51:20 -0500

    Updated the BMI calculator to tell you whether your BMI lands in a healthy range or not

--- bmi.py
+++ bmi.py
@@ -9,4 +9,15 @@ def calc_bmi(a, b):
 
 
 print("Your results are printed below: ")
-print(round(float(calc_bmi(weight, height)), 1))
+the_bmi = round(float(calc_bmi(weight, height)), 1)
+
+if(the_bmi < 18.5):
+    print("You are underweight.")
+elif(18.5 < the_bmi < 25):
+    print("You are in the normal range.")
+elif(25 < the_bmi < 30):
+    print("You are overweight.")
+elif(30 < the_bmi < 35):
+    print("You are obese")
+else:
+    print("You phat phuck")
