335626fa01ea9fbe355ab51c445eec0a191586fd
Lord Stinkle <drewwinkles@gmail.com>
2022-09-24 16:53:33 -0500

    Added a Trivia Class and connected MongoDB

---
+++ modules/level_one/lvl_one_questions.py
@@ -0,0 +1,10 @@
+class Question:
+    def __init__(self):
+        print('The questions are loaded')
+
+    quest_1 = {'1': 'How old is me?'}
+    ans_1= {'A': '31'}
+    quest_2 = {'2': 'What color is the sky?'}
+    ans_2 = {'A': 'blue'}
+    quest_3 = {'3': 'What are roses?'}
+    ans_3 = {'A': 'Red'}   
